51fb1c03ec9e6c2e38403c4320756e1c103a7dea
Rafiq <raw96@icloud.com>
2021-04-26 18:24:49 -0400

    final commit

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ const getTextData = () => {
 
 const getSong = () => {
 	let search = getTextData()
-	let song;
+	let songData;
 	const getTopTracks = $.get(`https://api.napster.com/v2.2/search?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&query=${search}&type=track`);
 	getTopTracks
-		.then(resp => resp.search.data.tracks[0].previewURL)
-		.then (data => song = data)
+		.then(resp =>  {
+			// resp.search.data.tracks[0].previewURL
+			return {
+				track: resp.search.data.tracks[0].previewURL,
+				album: resp.search.data.tracks[0].albumId
+			}
+		})
+		.then (data => songData = data)
 		.then(() => {
-			setSong(song)
+			setSong(songData.track)
+			setImage(songData.album)
 		})
 }
 
@@ -25,6 +32,11 @@ const setSong = (song) => {
 	$('#audio').attr("src", song).detach().appendTo('#audioButton')
 	audioButton.load()
 	audioButton.play()
+}
+
+const setImage = (album) => {
+	console.log()
+	$('#cover').attr("src",`http://direct.rhapsody.com/imageserver/v2/albums/${album}/images/300x300.jpg`)
 }
 
 button.addEventListener("click", getSong)
